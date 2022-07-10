aa65db075a3604f00cdb8dda925cbacb0c2846de
Emmanuel Correa <correaemmanuel.ec@gmail.com>
2022-07-10 17:12:07 -0300

    Desafio Array

--- Clase 6/Desafio/js/script.js
+++ Clase 6/Desafio/js/script.js
@@ -1,6 +1,4 @@
-// PRUEBA DE FUNCIONAMIENTO CARRITO BÁSICO
 const sumar = (num1, num2) => num1 + num2
-const restar = (num1, num2) => num1 - num2
 const multiplicar = (num1, num2) => num1 * num2
 const dividir = (num1, num2) => num1 / num2
 

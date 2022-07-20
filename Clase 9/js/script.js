166745e0e5b55ee691ecc7b68348f7c69a2a4e12
Emmanuel Correa <correaemmanuel.ec@gmail.com>
2022-07-20 20:53:21 -0300

    Clase 9 - Completa

--- Clase 9/js/script.js
+++ Clase 9/js/script.js
@@ -12,16 +12,89 @@
 
 
 
-const input1 = document.getElementById("input1")
-const inputColor = document.getElementById("inputColor")
 
-input1.addEventListener("input", () =>{
-    console.log(input1.value)
+// const input1 = document.getElementById("input1")
+// const inputColor = document.getElementById("inputColor")
+
+// input1.addEventListener("input", () =>{
+//     console.log(input1.value)
+// })
+
+// inputColor.addEventListener("input", () =>{
+//     document.body.style.backgroundColor = inputColor.value
+//     console.log(inputColor.value)
+// })
+
+
+
+
+
+
+// const input1 = document.getElementById("input1")
+// const botonBusqueda = document.getElementById("botonBusqueda")
+
+// input1.addEventListener("input", () =>{
+//     console.log(input1.value)
+// })
+
+// input1.addEventListener("change", () => {
+//     console.log(input1.value)
+// })
+
+// botonBusqueda.addEventListener("click", () => {
+//     console.log(input1.value)
+// })
+
+
+
+
+
+
+
+
+
+
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+}
+
+const users = []
+
+const idForm = document.getElementById("idForm")
+const divUsers = document.getElementById("divUsers")
+const botonUsers = document.getElementById("botonUsers")
+
+idForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    let username = document.getElementById("idUser").value
+    let email = document.getElementById("idEmail").value
+    let password = document.getElementById("idPassword").value
+
+    const user = new User(username, email, password)
+    users.push(user)
+    console.log(users)
+
+    idForm.reset()
 })
 
-inputColor.addEventListener("input", () =>{
-    document.body.style.backgroundColor = inputColor.value
-    console.log(inputColor.value)
+botonUsers.addEventListener("click", () => {
+    users.forEach(user => {
+        divUsers.innerHTML += `
+            <div class="card" styl="width: 18rem; margin:3px;">
+                <div class="card-body">
+                    <h5 class="card-title">${user.username}</h5>
+                    <p class="card-text">${user.email}</p>
+                </div>        
+            </div>
+        
+        `
+    })
 })
+
 
 

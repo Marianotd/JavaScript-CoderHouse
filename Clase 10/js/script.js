033b53106ffa8cdff5ebb5be2b4d69c9b6f323b6
Emmanuel Correa <correaemmanuel.ec@gmail.com>
2022-07-25 18:36:31 -0300

    Clase 10 - Completa

--- Clase 10/js/script.js
+++ Clase 10/js/script.js
@@ -6,41 +6,64 @@
 
 
 
-const botonDarkMode = document.getElementById("botonDarkMode")
-const botonLightMode = document.getElementById("botonLightMode")
+// const botonDarkMode = document.getElementById("botonDarkMode")
+// const botonLightMode = document.getElementById("botonLightMode")
 
-let darkMode
+// let darkMode
 
-if(localStorage.getItem("theme")){   
-    darkMode = localStorage.getItem("theme")
-} else {
-    localStorage.setItem("theme", "light")
+// if(localStorage.getItem("theme")){   
+//     darkMode = localStorage.getItem("theme")
+// } else {
+//     localStorage.setItem("theme", "light")
+// }
+
+// if(darkMode == "dark"){
+//     document.body.classList.add("darkMode")
+// }
+
+// botonDarkMode.addEventListener("click", () => {
+//     // document.body.style.backgroundColor = "#000"
+//     // document.body.style.color = "#fff"
+//     document.body.classList.add("darkMode")
+//     localStorage.setItem("theme", "dark")
+// })
+
+// botonLightMode.addEventListener("click", () => {
+//     // document.body.style.backgroundColor = "#fff"
+//     // document.body.style.color = "#000"
+//     document.body.classList.remove("darkMode")
+//     localStorage.setItem("theme", "light")
+// })
+
+
+
+
+
+
+
+class User {
+    constructor(username, email, password){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    loguearse(){
+        console.log("Hola, estoy logueado")
+    }
 }
 
-if(darkMode == "dark"){
-    document.body.classList.add("darkMode")
-}
+const user1 = new User("fran", "f@f.com", 1234)
+const user2 = new User("pepe", "p@p.com", 456)
+const user3 = new User("juan", "j@j.com", 789)
 
-botonDarkMode.addEventListener("click", () => {
-    // document.body.style.backgroundColor = "#000"
-    // document.body.style.color = "#fff"
-    document.body.classList.add("darkMode")
-    localStorage.setItem("theme", "dark")
-})
+const users = [user1, user2, user3]
 
-botonLightMode.addEventListener("click", () => {
-    // document.body.style.backgroundColor = "#fff"
-    // document.body.style.color = "#000"
-    document.body.classList.remove("darkMode")
-    localStorage.setItem("theme", "light")
-})
+localStorage.setItem("users", JSON.stringify(users))
 
+const array2 = JSON.parse(localStorage.getItem("users"))
 
-
-
-
-
-
+array2[0].loguearse()
 
 
 
